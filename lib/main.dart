0411c4427f0ef9c691f6f2c9ea5e3ae35ebb094c
AadhavanPL <aadhavanlenin@gmail.com>
2023-01-08 16:15:08 +0530

    layout done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(MaterialApp(
-    home: Morning(),
+    home: Night(),
   ));
 }
 
@@ -14,42 +14,38 @@ class Morning extends StatelessWidget {
   Widget build(BuildContext context) {
     double widthOfScreen = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/images/morning.png"),
-                        fit: BoxFit.cover)),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(45, 65, 0, 0),
-                child: Column(
-                  children: [
-                    const Text(
-                      'Good Morning, Aadhi',
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          color: Color(0xff190F0E),
-                          fontFamily: 'Poppins'),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                      child: Text(
-                        '7 January 2023',
-                        style: TextStyle(
-                            fontSize: 14.0,
-                            color: Color(0xff190F0E),
-                            fontFamily: 'Poppins'),
-                      ),
-                    )
-                  ],
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/morning.png"),
+                    fit: BoxFit.cover)),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(45, 65, 0, 0),
+            child: Column(
+              children: [
+                const Text(
+                  'Good Morning, Aadhi',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Color(0xff190F0E),
+                      fontFamily: 'Poppins'),
                 ),
-              ),
-            ],
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                  child: Text(
+                    '7 January 2023',
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        color: Color(0xff190F0E),
+                        fontFamily: 'Poppins'),
+                  ),
+                )
+              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
           ),
           Align(
             alignment: Alignment.bottomCenter,
@@ -61,14 +57,14 @@ class Morning extends StatelessWidget {
                   SizedBox(
                     child: IconButton(
                       onPressed: () {},
-                      icon: Image.asset('assets/images/sun.png'),
+                      icon: Image.asset('assets/images/sun_morning.png'),
                     ),
                     width: widthOfScreen / 2,
                   ),
                   SizedBox(
                     child: IconButton(
                       onPressed: () {},
-                      icon: Image.asset('assets/images/moon.png'),
+                      icon: Image.asset('assets/images/moon_morning.png'),
                     ),
                     width: widthOfScreen / 2,
                   ),
@@ -79,10 +75,76 @@ class Morning extends StatelessWidget {
           )
         ],
       ),
-      // bottomNavigationBar: BottomNavigationBar(items: [
-      //   BottomNavigationBarItem(icon: Image.asset('assets/images/morning.png')),
-      //   BottomNavigationBarItem(icon: Image.asset('assets/images/night.png'))
-      // ]),
+    );
+  }
+}
+
+class Night extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double widthOfScreen = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/night.png"),
+                    fit: BoxFit.cover)),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(45, 65, 0, 0),
+            child: Column(
+              children: [
+                const Text(
+                  'Good Night, Aadhi',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Color(0xff111312),
+                      fontFamily: 'Poppins'),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                  child: Text(
+                    '7 January 2023',
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        color: Color(0xff111312),
+                        fontFamily: 'Poppins'),
+                  ),
+                )
+              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 67,
+              width: widthOfScreen,
+              child: Row(
+                children: [
+                  SizedBox(
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Image.asset('assets/images/sun_night.png'),
+                    ),
+                    width: widthOfScreen / 2,
+                  ),
+                  SizedBox(
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Image.asset('assets/images/moon_night.png'),
+                    ),
+                    width: widthOfScreen / 2,
+                  ),
+                ],
+              ),
+              color: Color(0xff202425),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
