2ec6fbc2cd83a87b59f06e3b1c73ee1bd0be49b4
AadhavanPL <aadhavanlenin@gmail.com>
2023-01-08 15:42:52 +0530

    bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-      overlays: [SystemUiOverlay.bottom]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(MaterialApp(
     home: Morning(),
   ));
@@ -13,40 +12,71 @@ void main() {
 class Morning extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double widthOfScreen = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/morning.png"),
-                    fit: BoxFit.cover)),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(45, 65, 0, 0),
-            child: Column(
-              children: [
-                const Text(
-                  'Good Morning, Aadhi',
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      color: Color(0xff190F0E),
-                      fontFamily: 'Poppins'),
+          Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/morning.png"),
+                        fit: BoxFit.cover)),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(45, 65, 0, 0),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Good Morning, Aadhi',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Color(0xff190F0E),
+                          fontFamily: 'Poppins'),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Text(
+                        '7 January 2023',
+                        style: TextStyle(
+                            fontSize: 14.0,
+                            color: Color(0xff190F0E),
+                            fontFamily: 'Poppins'),
+                      ),
+                    )
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
                 ),
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                  child: Text(
-                    '7 January 2023',
-                    style: TextStyle(
-                        fontSize: 14.0,
-                        color: Color(0xff190F0E),
-                        fontFamily: 'Poppins'),
-                  ),
-                )
-              ],
-              crossAxisAlignment: CrossAxisAlignment.start,
-            ),
+              ),
+            ],
           ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 67,
+              width: widthOfScreen,
+              child: Row(
+                children: [
+                  SizedBox(
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Image.asset('assets/images/sun.png'),
+                    ),
+                    width: widthOfScreen / 2,
+                  ),
+                  SizedBox(
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Image.asset('assets/images/moon.png'),
+                    ),
+                    width: widthOfScreen / 2,
+                  ),
+                ],
+              ),
+              color: Color(0xff1D1111),
+            ),
+          )
         ],
       ),
       // bottomNavigationBar: BottomNavigationBar(items: [
@@ -56,14 +86,3 @@ class Morning extends StatelessWidget {
     );
   }
 }
-
-
-// void main() => runApp(MaterialApp(
-//       home: Scaffold(
-//           body: Container(
-//         decoration: BoxDecoration(
-//             image: DecorationImage(
-//                 image: AssetImage("assets/images/light.png"),
-//                 fit: BoxFit.cover)),
-//       )),
-//     ));
